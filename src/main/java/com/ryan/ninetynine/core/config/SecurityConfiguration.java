aa1f749ebebf1cfb9d040a8ba91337ec6085fa9f
TheGreatNobody <sun.ryan.it@gmail.com>
2024-11-05 22:35:18 +0800

    rename SecurityConfig to SecurityConfiguration

--- src/main/java/com/ryan/ninetynine/core/config/SecurityConfiguration.java
+++ src/main/java/com/ryan/ninetynine/core/config/SecurityConfiguration.java
@@ -1,25 +1,86 @@
 package com.ryan.ninetynine.core.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SecurityConfig {//implements WebMvcConfigurer {
-
+@EnableWebSecurity
+public class SecurityConfiguration {//implements WebMvcConfigurer {
+/**
+ * Spring Security 5.7.0-M2 deprecated the WebSecurityConfigurerAdapter
+ * 6.3 support for annotation parameters
+ * */
 //    @Override
 //    public void addViewControllers(ViewControllerRegistry registry) {
 //        registry.addRedirectViewController("/custom-path", "/swagger-ui.html");
+//    }
+
+
+        // [...]
+//        @Autowired
+//        public void configureGlobal(AuthenticationManagerBuilder auth)
+//                throws Exception {
+//            auth
+//                    .inMemoryAuthentication()
+//                    .withUser("user").password(passwordEncoder().encode("password")).roles("USER")
+//                    .and()
+//                    .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+//        }
+
+        @Bean
+        public BCryptPasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
+
+//@Override
+//protected void configure(final HttpSecurity http) throws Exception {
+//    http
+//            .formLogin()
+//            .loginPage("/login.html")
+//            .failureUrl("/login-error.html")
+//            .and()
+//            .logout()
+//            .logoutSuccessUrl("/index.html");
+//}
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(
+//            final AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
+
+//    @Bean
+//    public SecurityFilterChain configure(final HttpSecurity http) throws Exception {
+//        return http.cors(withDefaults())
+//                .csrf(withDefaults())
+//                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+//                .formLogin(form -> form
+//                        .loginPage("/login")
+//                        .usernameParameter("email")
+//                        .failureUrl("/login?loginError=true"))
+//                .logout(logout -> logout
+//                        .logoutSuccessUrl("/login?logoutSuccess=true")
+//                        .deleteCookies("JSESSIONID"))
+//                .exceptionHandling(exception -> exception
+//                        .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login?loginRequired=true")))
+//                .build();
 //    }
 
     @Bean
